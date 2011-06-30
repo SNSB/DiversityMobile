81973861f86a90ae74dc5a277de12763acbc0ed0
Georg Rollinger <theotherspriest@web.de>
2011-06-30 10:27:07 +0200

    init

---
+++ LinqToSQL/Model/StringISO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+
+namespace LinqToSQL.Model
+{
+    [Table]
+    public class StringISO
+    {
+        [Column]
+        public string Value { get; set; }
+
+        [Column(IsPrimaryKey=true)]
+        public Guid GUID { get; set; }
+
+
+    }
+}
