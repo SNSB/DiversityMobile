b2005c632071393a2bfec7b50a1206d951942094
Georg Rollinger <theotherspriest@web.de>
2011-09-01 10:51:06 +0200

    IU Pages & VMs

---
+++ DiversityPhone/ViewModels/EditIUVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace DiversityPhone.ViewModels
+{
+    public class EditIUVM : ReactiveObject
+    {
+        IMessageBus _messenger;
+        public EditIUVM(IMessageBus messenger)
+        {
+            _messenger = messenger;
+        }
+    }
+}
