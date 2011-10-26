9bdcb9defc48c2ebb678fea16b260392082031b7
Georg Rollinger <theotherspriest@web.de>
2011-10-26 11:39:41 +0200

    Boolean Operations on Observables Extenstion Methods

--- DiversityPhone/ViewModels/Extensions.cs
+++ DiversityPhone/ViewModels/Extensions.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 
 namespace DiversityPhone.ViewModels
 {
@@ -24,6 +25,32 @@ namespace DiversityPhone.ViewModels
                     return idx;
 
             return -1;
+        }
+
+        public static IObservable<bool> BooleanAnd(this IObservable<bool> This, params IObservable<bool>[] parameters)
+        {            
+            return This.CombineLatestMany((a, b) => a && b, parameters);
+        }
+
+        public static IObservable<bool> BooleanOr(this IObservable<bool> This, params IObservable<bool>[] parameters)
+        {
+            return This.CombineLatestMany((a, b) => a || b, parameters);
+        }
+
+        private static IObservable<T> CombineLatestMany<T>(this IObservable<T> This, Func<T, T, T> aggregator, params IObservable<T>[] parameters)
+        {
+            if (This == null)
+                throw new ArgumentNullException("This");
+            if (aggregator == null)
+                throw new ArgumentNullException("aggregator");
+
+            if (parameters != null)            
+            {
+                foreach (var parameter in parameters)
+                    if (parameter != null)
+                        This = This.CombineLatest(parameter, aggregator);
+            }
+            return This;
         }
     }
 }
