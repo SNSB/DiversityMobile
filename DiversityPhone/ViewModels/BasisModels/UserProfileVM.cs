6a903da34b3e943b4fe0c24c012474204a6a7848
Tobias Schneider <tobias.schneider@uni-bayreuth.de>
2011-11-02 20:47:49 +0100

    Updated ViewModels

---
+++ DiversityPhone/ViewModels/BasisModels/UserProfileVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using DiversityPhone.Model;
+using ReactiveUI.Xaml;
+using DiversityPhone.Messages;
+using System.Collections.Generic;
+
+namespace DiversityPhone.ViewModels.BasisModels
+{
+    public class UserProfileVM:ReactiveObject
+    {
+        IList<IDisposable> _subscriptions;
+        IMessageBus _messenger;
+
+        public ReactiveCommand Select { get; private set; }
+        public ReactiveCommand Edit { get; private set; }
+
+        public UserProfile Model { get; private set; }
+        public string Description { get { return Model.ToString(); } }
+        public Icon Icon { get; private set; }
+
+        public UserProfileVM(UserProfile model, IMessageBus messenger)
+        {
+            _messenger = messenger;
+            Model = model;
+            Icon = ViewModels.Icon.UserProfile;
+            _subscriptions = new List<IDisposable>()
+            {
+                (Select = new ReactiveCommand())
+                    .Subscribe(_ => _messenger.SendMessage<UserProfile>(Model,MessageContracts.SELECT)),
+                (Edit = new ReactiveCommand())
+                    .Subscribe(_ => _messenger.SendMessage<UserProfile>(Model,MessageContracts.EDIT)),
+            };
+        }
+
+    }
+}
---
+++ DiversityPhone/ViewModels/Edit/EditUserProfileVM.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+using DiversityPhone.Model;
+using ReactiveUI.Xaml;
+using DiversityPhone.Messages;
+using System.Collections.Generic;
+using DiversityPhone.Services;
+
+namespace DiversityPhone.ViewModels.Edit
+{
+    public class EditUserProfileVM:ReactiveObject
+    {
+        private IList<IDisposable> _subscriptions;
+
+        #region Services
+        private IMessageBus _messenger;
+        IOfflineStorage _storage;
+        #endregion
+
+        #region Commands
+        public ReactiveCommand Save { get; private set; }
+        public ReactiveCommand Edit { get; private set; }
+        public ReactiveCommand Delete { get; private set; }
+        #endregion
+
+        #region Properties
+        private UserProfile _Model;
+        public UserProfile Model
+        {
+            get { return _Model; }
+            set
+            {
+                this.RaiseAndSetIfChanged(x => x.Model, ref _Model, value);
+            }
+        }
+
+        public string _LoginName;
+        public string LoginName
+        {
+            get { return _AgentName; }
+        }
+
+        public string _AgentName;
+        public string AgentName
+        {
+            get { return _AgentName; }
+        } 
+
+        private bool _recordGeoPosition = false;
+        public bool RecordGeoPosition
+        {
+            get { return _recordGeoPosition; }
+            set { this.RaiseAndSetIfChanged(x => x.RecordGeoPosition, ref _recordGeoPosition, value); }
+        }
+
+        #endregion
+
+         public EditUserProfileVM(IMessageBus messenger, IOfflineStorage storage)
+        {
+
+            _messenger = messenger;
+            _storage = storage;
+
+
+            _subscriptions = new List<IDisposable>()
+            {
+                (Save = new ReactiveCommand())               
+                    .Subscribe(_ => executeSave()),
+
+                (Edit = new ReactiveCommand())
+                    .Subscribe(_ => enableEdit()),
+
+                (Delete = new ReactiveCommand())
+                    .Subscribe(_ => delete()),
+
+                _messenger.Listen<UserProfile>(MessageContracts.EDIT)
+                    .Subscribe(prof => updateView(prof))
+            };
+        }
+
+         private void executeSave()
+         {
+             updateModel();
+             _messenger.SendMessage<UserProfile>(Model, MessageContracts.SAVE);
+             _messenger.SendMessage<Message>(Message.NavigateBack);
+         }
+
+
+         private void enableEdit()
+         {
+         }
+
+         private void delete()
+         {
+             _messenger.SendMessage<UserProfile>(Model, MessageContracts.DELETE);
+             _messenger.SendMessage<Message>(Message.NavigateBack);
+         }
+
+         private void updateModel()
+         {
+             Model.RecordGeoPosition = this.RecordGeoPosition;
+         }
+
+         private void updateView(UserProfile prof)
+         {
+             this.Model = prof;
+             this.RecordGeoPosition = prof.RecordGeoPosition;
+             this._LoginName = prof.LoginName;
+             this._AgentName = prof.AgentName;
+
+         }
+
+    }
+}
