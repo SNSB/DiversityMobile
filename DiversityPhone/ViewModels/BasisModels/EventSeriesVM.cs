53f1bf86fd8e6344748bc39733bd19b3c54f46f8
Georg Rollinger <theotherspriest@web.de>
2011-12-08 11:43:45 +0100

    NoEventSeries

--- DiversityPhone/ViewModels/BasisModels/EventSeriesVM.cs
+++ DiversityPhone/ViewModels/BasisModels/EventSeriesVM.cs
@@ -21,12 +21,12 @@
         public EventSeries Model
         {
             get { return _Model; }
-            set
+            private set
             {
                 this.RaiseAndSetIfChanged(x => x.Model, ref _Model, value);
             }
         }
-        //public EventSeries Model { get; private set; }
+        
 
         public string Description { get { return Model.Description; } }
         public Icon Icon { get; private set; }
@@ -39,21 +39,24 @@
             if (EventSeries.isNoEventSeries(model)) //Überprüfen auf NoEventSeries
                 Icon = ViewModels.Icon.NoEventSeries;
             else
+            {
                 Icon = ViewModels.Icon.EventSeries;
 
-            _subscriptions = new List<IDisposable>()
-            {
-                (Select = new ReactiveCommand())
-                    .Subscribe(_ =>
-                        {
-                            _messenger.SendMessage<NavigationMessage>(new NavigationMessage(Page.ViewES, Model.SeriesID.ToString()));
-                        }),
-                (Edit = new ReactiveCommand())
-                    .Subscribe(_ => 
-                        {
-                            _messenger.SendMessage<NavigationMessage>(new NavigationMessage(Page.EditES, Model.SeriesID.ToString()));
-                        }),
-            };
+                //NoEventSeries nicht editierbar/selektierbar
+                _subscriptions = new List<IDisposable>()
+                {
+                    (Select = new ReactiveCommand())
+                        .Subscribe(_ =>
+                            {
+                                _messenger.SendMessage<NavigationMessage>(new NavigationMessage(Page.ViewES, Model.SeriesID.ToString()));
+                            }),
+                    (Edit = new ReactiveCommand())
+                        .Subscribe(_ => 
+                            {
+                                _messenger.SendMessage<NavigationMessage>(new NavigationMessage(Page.EditES, Model.SeriesID.ToString()));
+                            }),
+                };
+            }
         }
     }
 }
