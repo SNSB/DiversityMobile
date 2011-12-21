cf4c921006c502e8096dc165bb65de7d6351f61a
unknown <Georg@.(none)>
2011-12-21 17:35:11 +0100

    ElementVMBase Documentation

--- DiversityPhone/ViewModels/ElementVMBase.cs
+++ DiversityPhone/ViewModels/ElementVMBase.cs
@@ -13,15 +13,39 @@ using ReactiveUI.Xaml;
 
 namespace DiversityPhone.ViewModels
 {
+    /// <summary>
+    /// Base class for all Element ViewModels (VMs, that represent a single item of a Model Class).    /// 
+    /// </summary>
+    /// <typeparam name="T">The Model class that this VM will encapsulate.</typeparam>
     public abstract class ElementVMBase<T>
     {
+        /// <summary>
+        /// Preinitialized Messenger Instance 
+        /// </summary>
         protected IMessageBus Messenger { get; private set; }
 
+        /// <summary>
+        /// Operation: Select (Go To ViewXX Page)
+        /// </summary>
         public ReactiveCommand Select { get; protected set; }
+        /// <summary>
+        /// Operation: Edit (Go To EditXX Page)
+        /// </summary>
         public ReactiveCommand Edit { get; protected set; }
 
+        /// <summary>
+        /// Encapsulated Model Instance
+        /// </summary>
         public T Model { get; private set; }
+
+        /// <summary>
+        /// String to Display for this Object
+        /// </summary>
         public abstract string Description { get; }
+
+        /// <summary>
+        /// Icon to display for this Object
+        /// </summary>
         public abstract Icon Icon { get; }
 
 
