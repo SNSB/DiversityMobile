9a681caeaa73246443e715c3b3fc809fff43d1e5
Georg Rollinger <theotherspriest@web.de>
2012-01-17 17:27:00 +0100

    DataContract superfluous.

--- DiversityPhone/Services/PageState.cs
+++ DiversityPhone/Services/PageState.cs
@@ -9,45 +9,39 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 
 namespace DiversityPhone.Services
 {
-    [DataContractAttribute]
+    
     public class PageState 
     {
         /// <summary>
         /// GUID string uniquely identifying this Page visit
-        /// </summary>
-        [DataMember]
+        /// </summary>        
         public string Token { get; set; }
         
         /// <summary>
         /// Information that should be displayed by the visited Page
         /// e.g. ID of the object that the user wants to edit etc...
-        /// </summary>
-        [DataMember]
+        /// </summary>        
         public string Context { get;  set; }
 
         /// <summary>
         /// Type of the Referrer (Object that originated the Request)
-        /// </summary>
-        [DataMember]
+        /// </summary>        
         public ReferrerType ReferrerType { get;  set; }
 
         /// <summary>
         /// Information that shows the Origin of the Request, if needed
         /// e.g. ID of the parent object
-        /// </summary>
-        [DataMember]
+        /// </summary>        
         public string Referrer { get;  set; }
 
         /// <summary>
         /// State Dictionary, that should be used by the Page to persist custom state
         /// (internal to the Page)
-        /// </summary>
-        [DataMember]
+        /// </summary>        
         public IDictionary<string, string> State { get; set; }
 
         public PageState(string token, string context, ReferrerType refType = ReferrerType.None, string referrer = null)
