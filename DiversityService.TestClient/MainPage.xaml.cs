ecf5d86413ec43f8bed2f34d6604c8e6bd5f061a
Georg Rollinger <rthundr@live.de>
2012-02-09 19:04:32 +0100

    TestClient Project

---
+++ DiversityService.TestClient/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace DiversityService.TestClient
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var svc = new ServiceReference1.DiversityServiceClient();
+            svc.ClientCredentials.UserName.UserName = "Rollinger";
+            svc.ClientCredentials.UserName.Password = "Rolli#2-AI4@UBT";
+
+            svc.GetUserInfoCompleted += (s,args) => 
+            { 
+                System.Diagnostics.Debug.WriteLine(args.Result.AgentUri);
+            };
+            svc.GetUserInfoAsync();
+        }
+        
+    }
+}
