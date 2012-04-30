15b4bea7d7f4beab3234f5723d44ef13078dc4c1
Georg Rollinger <s1geroll@stmail.uni-bayreuth.de>
2012-04-30 13:11:04 +0200

    BackgroundService #1

---
+++ DiversityPhone/Services/BackgroundService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using DiversityPhone.Messages;
+using System.Collections.Generic;
+
+namespace DiversityPhone.Services
+{
+    public class BackgroundService
+    {
+        List<IBackgroundProcess> waitingProcs = new List<IBackgroundProcess>();
+        IBackgroundProcess runningProc = null;
+
+        public BackgroundService(IMessageBus msngr)
+        {
+            msngr.Listen<IBackgroundProcess>(MessageContracts.START)
+                .Subscribe(proc => startProcess(proc));
+
+        }
+
+        public void startProcess(IBackgroundProcess proc)
+        {
+            lock (this)
+            {
+                waitingProcs.Add(proc);
+            }
+            
+        }
+
+        private void nextProcess()
+        {
+            
+            lock (this)
+            {
+                if (runningProc == null && waitingProcs.Count > 0)
+                {
+                    runningProc = waitingProcs[0];
+                    waitingProcs.RemoveAt(0);
+                    runningProc.Run();                   
+                }
+            }
+            
+        }
+
+        
+    }
+}
---
+++ DiversityPhone/Services/IBackgroundProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiversityPhone.Services
+{
+    public interface IBackgroundProcess
+    {
+        string ProcessName { get; }
+        void Run();
+        void Cancel();
+
+    }
+}
