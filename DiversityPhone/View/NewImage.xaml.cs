aef6d54358f073f7d7df02b00cee8a6bdf5de914
Tobias Schneider <tobias.schneider@uni-bayreuth.de>
2012-06-19 11:44:29 +0200

    Refactoring Photo taking

--- DiversityPhone/View/NewImage.xaml.cs
+++ DiversityPhone/View/NewImage.xaml.cs
@@ -6,6 +6,11 @@ using System.Xml.Linq;
 using DiversityPhone.ViewModels;
 using DiversityPhone.Model;
 using System.Windows.Data;
+using System.Windows;
+using Microsoft.Devices;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using Microsoft.Xna.Framework.Media;
 
 namespace DiversityPhone.View
 {
@@ -19,10 +24,62 @@ namespace DiversityPhone.View
                 return DataContext as NewImageVM;
             }
         }
+        private NewImageAppBarUpdater _appbar;
 
         public NewImage()
         {
             InitializeComponent();
+            _appbar = new NewImageAppBarUpdater(this.ApplicationBar, VM);
+
+            this.CorrectViewFinderOrientation(this.Orientation);
+            this.OrientationChanged += new EventHandler<OrientationChangedEventArgs>(NewImagePage_OrientationChanged);
+
         }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e) //Einbinden
+        {
+
+            VM.Camera.Initialized += new EventHandler<CameraOperationCompletedEventArgs>(Camera_Initialized);
+            VM.Camera.AutoFocusCompleted += new EventHandler<CameraOperationCompletedEventArgs>(Camera_AutoFocusCompleted);
+            this.cameraViewBrush.SetSource(VM.Camera);
+
+            this.CorrectViewFinderOrientation(this.Orientation);
+            this.OrientationChanged += new EventHandler<OrientationChangedEventArgs>(NewImagePage_OrientationChanged);
+        }
+
+        private void Camera_Initialized(object sender, CameraOperationCompletedEventArgs e)//Einbinden
+        {
+
+            if (VM.Camera.IsFlashModeSupported(FlashMode.Auto))
+                VM.Camera.FlashMode = FlashMode.Auto;
+           
+        }
+
+        private void Camera_AutoFocusCompleted(object sender, CameraOperationCompletedEventArgs e) //Einbinden
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                this.cameraView.BorderBrush = new SolidColorBrush(Colors.Red);
+            });
+        }
+
+
+        #region Orientation
+        private void NewImagePage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            this.CorrectViewFinderOrientation(e.Orientation);
+        }
+
+        private void CorrectViewFinderOrientation(PageOrientation orientation)
+        {
+            if (orientation == PageOrientation.LandscapeLeft)
+                this.cameraViewBrushTransform.Rotation = VM.Camera.Orientation - 90.0;
+            else if (orientation == PageOrientation.LandscapeRight)
+                this.cameraViewBrushTransform.Rotation = VM.Camera.Orientation + 90.0;
+        }
+        #endregion
+
+       
+
     }
 }
--- DiversityPhone/ViewModels/Multimedia/NewImageVM.cs
+++ DiversityPhone/ViewModels/Multimedia/NewImageVM.cs
@@ -1,18 +1,233 @@
 ﻿using System;
-using System.Net;
+using ReactiveUI;
+using ReactiveUI.Xaml;
+using DiversityPhone.Model;
+using DiversityPhone.Messages;
+using System.Reactive.Linq;
+using DiversityPhone.Services;
+using System.Collections.Generic;
+using Funq;
+using System.Windows.Media.Imaging;
+using System.IO.IsolatedStorage;
+using System.IO;
+using Microsoft.Devices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace DiversityPhone.ViewModels
 {
-    public class NewImageVM
+    public class NewImageVM : EditElementPageVMBase<MultimediaObject>
     {
+
+        public PhotoCamera Camera { get; private set; }
+
+        #region Properties
+
+        private string _Uri;
+        public string Uri
+        {
+            get { return _Uri; }
+            set { this.RaiseAndSetIfChanged(x => x.Uri, ref _Uri, value); }
+        }
+
+
+        private BitmapImage _oldImage;
+
+        public BitmapImage OldImage
+        {
+            get
+            {
+                return _oldImage;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(x => x.OldImage, ref _oldImage, value);
+            }
+        }
+
+        private BitmapImage _actualImage;
+
+        public BitmapImage ActualImage
+        {
+            get
+            {
+                return _actualImage;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(x => x.ActualImage, ref _actualImage, value);
+            }
+        }
+
+        private bool _shootingEnabled = true;
+        public bool ShootingEnabled //Converter for image and vidfeoBrush in View needed
+        {
+            get
+            {
+                return _shootingEnabled;
+            }
+            set
+            {
+                this.aiseAndSetIfChanged(x => x.ShootingEnabled, ref _shootingEnabled, value);
+                //Adjust appbar which is in View
+            }
+        }
+        public bool ShootingDisabled //For DataBinding
+        {
+            get
+            {
+                return !_shootingEnabled;
+            }
+        }
+
+        #endregion 
+
+        #region Commands
+
+        public ReactiveCommand Reset { get; private set; }
+        public ReactiveCommand Take { get; private set; } //Action depending on ShootingEnabled
+        //public ReactiveCommand Crop { get; private set; }
+
+        #endregion
+
+
+        public NewImageVM()
+        {
+            Reset = new ReactiveCommand();
+            Reset.Subscribe(_ => refresh());
+            Take = new ReactiveCommand();
+            Take.Subscribe(_ => take());
+            this.initializeCamera();
+
+        }
+
+        private void initializeCamera()
+        {
+            if (PhotoCamera.IsCameraTypeSupported(CameraType.Primary))
+                this.Camera = new PhotoCamera(CameraType.Primary);
+            else
+            {
+                MessageBox.Show("Cannot find a camera on this device");
+                return;
+            }
+            this.Camera.CaptureImageAvailable += new EventHandler<ContentReadyEventArgs>(Camera_CaptureImageAvailable);
+        }
+
+        protected override void UpdateModel()
+        {
+            saveImage();
+            Current.Model.LogUpdatedWhen = DateTime.Now;
+            Current.Model.Uri = Uri;
+        }
+
+
+        protected override void OnSave()
+        {
+            
+        }
+
+        public override void SaveState()
+        {
+            base.SaveState();
+        }
+
+        protected override MultimediaObject ModelFromState(Services.PageState s)
+        {
+            if (s.Referrer != null)
+            {
+                int parent;
+                if (int.TryParse(s.Referrer, out parent))
+                {
+                    return new MultimediaObject()
+                    {
+                        RelatedId = parent,
+                        OwnerType = s.ReferrerType,
+                        MediaType=MediaType.Image,
+                    };
+                }
+            }
+
+            return null;
+        }
+
+        protected override IObservable<bool> CanSave()
+        {
+            return this.ObservableForProperty(x => x.Uri)
+                .Select(uri=> !string.IsNullOrEmpty(uri.Value))
+                .StartWith(false);
+        }
+
+        protected override ElementVMBase<MultimediaObject> ViewModelFromModel(MultimediaObject model)
+        {
+            return new MultimediaObjectVM(model);
+        }
+
+        private void take()
+        {
+            if (ShootingEnabled)
+            {
+                this.Camera.CaptureImage();
+            }
+            else
+                ShootingEnabled = true;
+        }
+
+        private void Camera_CaptureImageAvailable(object sender, ContentReadyEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                BitmapImage image = new BitmapImage();
+                image.SetSource(e.ImageStream);
+                this.OldImage = ActualImage;
+                this.ActualImage = image;
+                this.ShootingEnabled = false;
+            });
+        }
+
+        private void refresh()
+        {
+            if(OldImage!=null)
+                ActualImage = OldImage;
+        }
+
+    
+
+        private void saveImage()
+        {
+            //Save Picture
+            //
+
+            //Make progress bar visible for the event handler as there may be posible latency.
+            progressBar1.Visibility = Visibility.Visible; //Work with iBusy and AsyncCommands
+
+            //Create filename for JPEG in isolated storage as a Guid and filename for thumb
+            String uri;
+
+            if (Current.Model.Uri == null || Current.Model.Uri.Equals(String.Empty))
+            {
+                Guid g = Guid.NewGuid();
+                uri = g.ToString() + ".jpg";
+            }
+            else
+            {
+                uri = Current.Model.Uri;
+            }
+            //Create virtual store and file stream. Check for duplicate tempJPEG files.
+            var myStore = IsolatedStorageFile.GetUserStoreForApplication();
+            if (myStore.FileExists(uri))
+            {
+                myStore.DeleteFile(uri);
+            }
+            IsolatedStorageFileStream myFileStream = myStore.CreateFile(uri);
+            //Encode the WriteableBitmap into JPEG stream and place into isolated storage.
+            System.Windows.Media.Imaging.Extensions.SaveJpeg(new WriteableBitmap(this.ActualImage), myFileStream, this.ActualImage.PixelWidth, this.ActualImage.PixelHeight, 0, 85);
+            myFileStream.Close();
+            if (Current.Model.Uri == null || Current.Model.Uri.Equals(String.Empty))
+            {
+                this.Uri = uri;
+
+            }
+            progressBar1.Visibility = Visibility.Collapsed;
+        }
 
     }
 }
