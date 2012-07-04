1dfedcfaf81b368ec8a6fcca7fa9b13fd5b59600
Tobias Schneider <tobias.schneider@uni-bayreuth.de>
2012-07-04 11:33:35 +0200

    Just DiversityServiceSource

---
+++ DiversityService/SyncException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiversityService
+{
+    class SyncException : Exception
+    {
+        public SyncException(String message)
+            : base(message)
+        {
+        }
+    }
+}
