6c5e722be35891fbf78906b090bf26919eba0684
Georg Rollinger <theotherspriest@web.de>
2012-08-16 15:35:53 +0200

    locsvc #1

--- DiversityPhone/Services/LocationService.cs
+++ DiversityPhone/Services/LocationService.cs
@@ -14,6 +14,7 @@ namespace DiversityPhone.Services
     using ReactiveUI;
     using NLog;
 using System.Reactive.Disposables;
+using System.Collections.Generic;
 
     /// <summary>
     /// The location service, uses reactive extensions to publish the current location.
@@ -31,7 +32,10 @@ using System.Reactive.Disposables;
         // Defines the default accuracy to be used.
         private readonly GeoPositionAccuracy defaultAccuracy = GeoPositionAccuracy.Default;
 
-        CompositeDisposable location_subscriptions = new CompositeDisposable();
+        private GeoCoordinateWatcher watcher = null;
+        private RefCountDisposable watcher_refcount = null;
+        private ISubject<GeoPositionStatus> status_subject = new BehaviorSubject<GeoPositionStatus>(GeoPositionStatus.Disabled);
+        private ISubject<GeoPosition<GeoCoordinate>> coordinate_subject = new BehaviorSubject<GeoPosition<GeoCoordinate>>(new GeoPosition<GeoCoordinate>(DateTimeOffset.Now, GeoCoordinate.Unknown));
 
         private bool _IsEnabled = true;
         public bool IsEnabled 
@@ -52,6 +56,10 @@ using System.Reactive.Disposables;
         public LocationService() 
         {            
             this.log = this.Log();
+            this.watcher = new GeoCoordinateWatcher(defaultAccuracy);
+
+            this.watcher.PositionChanged += (s, args) => coordinate_subject.OnNext(args.Position);
+            this.watcher.StatusChanged += (s, args) => status_subject.OnNext(args.Status);
         }
 
         /// <summary>
@@ -96,8 +104,7 @@ using System.Reactive.Disposables;
             this.log.Debug("LocationService: Accuracy (default) - {0} seconds", this.defaultAccuracy);
             this.log.Debug("LocationService: Timeout - {0} seconds", locationTimeout.TotalSeconds);
 
-             return this.LocationImpl(this.defaultAccuracy, locationTimeout)
-                 .Select(l => l);
+            return this.CurrentLocationByTimeImpl(locationTimeout);
         }
 
         /// <summary>
@@ -115,8 +122,7 @@ using System.Reactive.Disposables;
             this.log.Debug("LocationService: LocationByTimeThreshold...");
             this.log.Debug("LocationService: Frequency - '{0}'", frequency);
 
-            return this.LocationByTimeImpl(frequency, this.defaultAccuracy)
-                    .Select(l => l);
+            return this.LocationByTimeImpl(frequency);
         }
 
         /// <summary>
@@ -133,196 +139,96 @@ using System.Reactive.Disposables;
             this.log.Debug("LocationService: LocationByDistanceThreshold...");
             this.log.Debug("LocationService: Distance - '{0}'", distance);
 
-            return this.LocationByDistanceThresholdImpl(distance, this.defaultAccuracy)
-                .Select(l => l);
+            return this.LocationByDistanceThresholdImpl(distance);                
         }
 
-        private IObservable<GeoCoordinate> LocationByDistanceThresholdImpl(int distance, GeoPositionAccuracy accuracy)
+        private IObservable<GeoCoordinate> LocationByDistanceThresholdImpl(int distance)
         {
-            var subject = new BehaviorSubject<GeoCoordinate>(GeoCoordinate.Unknown);
+            var watcher_handle = StartWatcherIfNecessary();
+            var comparer = new DistanceThresholdComparer(distance);
 
-            try
-            {
-                var watcher = new GeoCoordinateWatcher(accuracy) { MovementThreshold = distance };
-                watcher.PositionChanged += (o, args) =>
-                {
-                    var newLocation = args.Position.Location;
-                    this.log.Debug("LocationService: New location(distance) - {0}, {1}", newLocation.Latitude, newLocation.Longitude);
-                    subject.OnNext(newLocation);
-                };
-
-                Scheduler.ThreadPool.Schedule(() =>
-                {
-                    var started = watcher.TryStart(true, DefaultStartupTimeout);
-                    if (!started)
-                    {
-                        subject.OnError(new Exception("Failed to start GeoCoordinateWatcher!"));
-                    }
-                });
-
-                return subject.Where(c => !c.IsUnknown)
-                    .DistinctUntilChanged()
-                    .Finally(() =>
-                     {
-                        this.log.Debug("LocationService: Shutting down watcher...");
-
-                        watcher.Stop();
-                    })
-                    .AsObservable();
-            }
-            catch (Exception exn)
-            {
-                this.log.Debug("LocationService: Failed LocationByDistanceThresholdImpl, message - '{0}'", exn.Message);
-                subject.OnError(exn);
-
-                return subject.AsObservable();
-            }
+            return coordinate_subject
+                .Select(p => p.Location)
+                .DistinctUntilChanged(comparer)
+                .Finally(() => watcher_handle.Dispose())
+                .AsObservable();
         }
 
-        private GeoCoordinate CurrentLocationByTimeImpl(TimeSpan timeSpan, GeoPositionAccuracy accuracy)
+        private GeoCoordinate CurrentLocationByTimeImpl(TimeSpan timeSpan)
         {
-            GeoCoordinateWatcher watcher = null;
+            var watcher_handle = StartWatcherIfNecessary();
 
-            try
-            {
-                var currentLocation = GeoCoordinate.Unknown;
-                watcher = new GeoCoordinateWatcher(accuracy);
-                
-                this.log.Debug("LocationService: Start time - " + DateTime.Now.ToUniversalTime());
-                if (watcher.TryStart(true, timeSpan))
-                {
-                    currentLocation = watcher.Position.Location;
-                    this.log.Debug("LocationService: Location calculated - " + DateTime.Now.ToUniversalTime());
-                    this.log.Debug("LocationService: New location(time) - {0}, {1}", currentLocation.Latitude, currentLocation.Longitude);
-                }
-                else
-                {
-                    this.log.Debug("LocationService: Location NOT calculated");
-                }
-                
-                return currentLocation;
-            }
-            catch (Exception exn)
-            {
-                this.log.Debug("LocationService: Failed CurrentLocationByTimeImpl, message - '{0}'", exn.Message);
+            return coordinate_subject.Select(p => p.Location).Where(l => !l.IsUnknown)
+                .Timeout(timeSpan, Observable.Return(GeoCoordinate.Unknown))
+                .Finally(() => watcher_handle.Dispose())
+                .First();
+        }      
 
-                return GeoCoordinate.Unknown;
-            }
-            finally
-            {
-                if (watcher != null)
-                {
-                    watcher.Stop();
-                }
-            }
-        }
-
-        private IObservable<GeoCoordinate> LocationImpl(GeoPositionAccuracy accuracy, TimeSpan locationTimeout)
+        private IObservable<GeoCoordinate> LocationByTimeImpl(TimeSpan timeSpan)
         {
-            var subject = new Subject<GeoCoordinate>();
+            var watcher_handle = StartWatcherIfNecessary();
 
-            try
-            {
-                var watcher = new GeoCoordinateWatcher(accuracy);
-                watcher.PositionChanged += (o, args) =>
-                {
-                    var newLocation = args.Position.Location;
-                    this.log.Debug("LocationService: New location(current) - {0}: {1}, {2}", args.Position.Timestamp, newLocation.Latitude, newLocation.Longitude);
-
-                    if ((DateTime.Now - args.Position.Timestamp.DateTime) < locationTimeout)
-                    {
-                        subject.OnNext(newLocation);
-                        subject.OnCompleted();
-                    }
-                };
-
-                Scheduler.ThreadPool.Schedule(() =>
-                {
-                    var started = watcher.TryStart(true, DefaultStartupTimeout);
-                    if (!started)
-                    {
-                        subject.OnError(new Exception("Failed to start GeoCoordinateWatcher!"));
-                    }
-                });
-
-                return subject.AsObservable()
-                    .Where(c => !c.IsUnknown)
-                    .Finally(() =>
-                    {
-                        this.log.Debug("LocationService: Shutting down watcher...");
-                    
-                        watcher.Stop();
-                    });
-            }
-            catch (Exception exn)
-            {
-                this.log.Debug("LocationService: Failed LocationImpl, message - '{0}'", exn.Message);
-                subject.OnError(exn);
-
-                return subject.AsObservable();
-            }
-        }
-
-        private IObservable<GeoCoordinate> LocationByTimeImpl(TimeSpan timeSpan, GeoPositionAccuracy accuracy)
-        {
-            try
-            {
-                return Observable.Interval(timeSpan)
-                    .Select(t => this.CurrentLocationByTimeImpl(timeSpan, accuracy))
-                    .Where(c => !c.IsUnknown)
-                    .Finally(() => this.log.Debug("LocationService: Shutting down watcher..."))
-                    .DistinctUntilChanged();
-            }
-            catch (Exception exn)
-            {
-                this.log.Debug("LocationService: Failed LocationByTimeImpl, message - '{0}'", exn.Message);
-                
-                return Observable.Return(GeoCoordinate.Unknown);
-            }
-        }
+            return Observable.Throttle(coordinate_subject.Select(p => p.Location), timeSpan)
+                .Where(c => !c.IsUnknown)
+                .Finally(() => watcher_handle.Dispose())
+                .DistinctUntilChanged();
+        }   
 
         private IObservable<GeoPositionStatus> StatusImpl()
+        {     
+            var watcher_handle = StartWatcherIfNecessary();
+
+            var valid_status_seen = false;
+
+            return status_subject
+                .TakeWhile(s => (valid_status_seen |= (s != GeoPositionStatus.Initializing && s != GeoPositionStatus.NoData)))
+                .Finally(() => watcher_handle.Dispose())
+                .AsObservable();               
+        }
+
+        IDisposable StartWatcherIfNecessary()
         {
-            var subject = new Subject<GeoPositionStatus>();
-
-            try
+            lock (this)
             {
-                var watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
-                watcher.StatusChanged += (o, args) =>
+                if (watcher_refcount == null)
                 {
-                    this.log.Debug("LocationService: New Status - '{0}'", args.Status);
-                    subject.OnNext(args.Status);
+                    watcher_refcount = new RefCountDisposable(Disposable.Create(() => watcher.Stop()));
 
-                    if (args.Status != GeoPositionStatus.Initializing &&
-                        args.Status != GeoPositionStatus.NoData)
+                    Scheduler.ThreadPool.Schedule(() =>
                     {
-                        subject.OnCompleted();
-                    }
-                };
-
-                Scheduler.ThreadPool.Schedule(() =>
-                {
-                    var started = watcher.TryStart(true, DefaultStartupTimeout);
-                    if (!started)
-                    {
-                        subject.OnError(new Exception("Failed to start GeoCoordinateWatcher!"));
-                    }
-                });
-
-                return subject.AsObservable()
-                    .Finally(() =>
-                    {
-                        this.log.Debug("LocationService: Shutting down watcher...");
-
-                        watcher.Stop();
+                        var started = watcher.TryStart(true, DefaultStartupTimeout);
+                        if (!started)
+                        {
+                            coordinate_subject.OnError(new Exception("Failed to start GeoCoordinateWatcher!"));
+                            status_subject.OnError(new Exception("Failed to start GeoCoordinateWatcher!"));
+                        }
                     });
-            }
-            catch (Exception exn)
-            {
-                this.log.Debug("LocationService: Failed StatusImpl, message - '{0}'", exn.Message);
-                subject.OnError(exn);
+                }
 
-                return subject.AsObservable();
+                return watcher_refcount.GetDisposable();
+            }
+        }
+
+        private class DistanceThresholdComparer : IEqualityComparer<GeoCoordinate>
+        {
+            double distance_threshold;
+
+            public DistanceThresholdComparer( double threshold )
+            {
+                if (threshold < 0.0)
+                    throw new ArgumentOutOfRangeException("threshold");
+
+                distance_threshold = threshold;
+            }
+
+            public bool Equals(GeoCoordinate x, GeoCoordinate y)
+            {
+                return x.GetDistanceTo(y) < distance_threshold;
+            }
+
+            public int GetHashCode(GeoCoordinate obj)
+            {
+                return obj.GetHashCode();
             }
         }
     }
