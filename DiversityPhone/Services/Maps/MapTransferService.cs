8a6a1332fd7bbf4d45c32b065cc166827492f622
Georg Rollinger <theotherspriest@web.de>
2012-08-20 17:03:22 +0200

    maptransferservice impl #1

--- DiversityPhone/Services/Maps/MapTransferService.cs
+++ DiversityPhone/Services/Maps/MapTransferService.cs
@@ -15,6 +15,7 @@ using System.IO;
 using DiversityPhone.Model;
 using System.Collections.Generic;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 
 namespace DiversityPhone.Services
 {
@@ -22,9 +23,11 @@ namespace DiversityPhone.Services
     {
 
         private PhoneMediaServiceClient _mapinfo = new PhoneMediaServiceClient();
-        private HttpWebRequest _imageHttp;
-        private HttpWebRequest _xmlHttp;
+        
 
+        private IObservable<GetMapListFilterCompletedEventArgs> GetMapsListCompletedObservable;
+        private IObservable<GetMapUrlCompletedEventArgs> GetMapUrlCompletedObservable;
+        private IObservable<GetXmlUrlCompletedEventArgs> GetXmlUrlCompletedObservable;
 
         public MapTransferService()            
         {
@@ -35,6 +38,10 @@ namespace DiversityPhone.Services
                     isoStore.CreateDirectory("Maps");
                 }
             }
+
+            GetMapsListCompletedObservable = Observable.FromEvent<EventHandler<GetMapListFilterCompletedEventArgs>, GetMapListFilterCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetMapListFilterCompleted += d, d => _mapinfo.GetMapListFilterCompleted -= d);
+            GetMapUrlCompletedObservable = Observable.FromEvent<EventHandler<GetMapUrlCompletedEventArgs>, GetMapUrlCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetMapUrlCompleted += d, d => _mapinfo.GetMapUrlCompleted -= d);                
+            GetXmlUrlCompletedObservable = Observable.FromEvent<EventHandler<GetXmlUrlCompletedEventArgs>, GetXmlUrlCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetXmlUrlCompleted += d, d => _mapinfo.GetXmlUrlCompleted -= d);               
         }
 
 
@@ -43,10 +50,11 @@ namespace DiversityPhone.Services
 
         public IObservable<IEnumerable<String>> GetAvailableMaps(String searchString)
         {
-            var source = Observable.FromEvent<EventHandler<GetMapListFilterCompletedEventArgs>, GetMapListFilterCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetMapListFilterCompleted += d, d => _mapinfo.GetMapListFilterCompleted -= d)
-                .Select(args => args.Result as IEnumerable<String>);
-            var res = singleResultObservable(source);
-            _mapinfo.GetMapListFilterAsync(searchString);
+            var res = singleResultObservable(
+                GetMapsListCompletedObservable
+                .Where(args => args.UserState == searchString)
+                .Select(args => args.Result as IEnumerable<string>));
+            _mapinfo.GetMapListFilterAsync(searchString, searchString);
             return res;
         }
 
@@ -58,24 +66,28 @@ namespace DiversityPhone.Services
 
         public IObservable<Map> downloadMap(String serverKey)
         {
-            var mapUrl = Observable.FromEvent<EventHandler<GetMapUrlCompletedEventArgs>, GetMapUrlCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetMapUrlCompleted += d, d => _mapinfo.GetMapUrlCompleted -= d)
-                .Select(args => args.Result as String);
-            var xmlUrl = Observable.FromEvent<EventHandler<GetXmlUrlCompletedEventArgs>, GetXmlUrlCompletedEventArgs>((a) => (s, args) => a(args), d => _mapinfo.GetXmlUrlCompleted += d, d => _mapinfo.GetXmlUrlCompleted -= d)
-               .Select(args => args.Result as String);
-            _mapinfo.GetMapUrlAsync(serverKey);
-            _mapinfo.GetXmlUrlAsync(serverKey);
+            Observable.Merge(
+                GetMapUrlCompletedObservable
+                .Where(args => args.UserState == serverKey)
+                .Select(args => args.Result),                
+                GetXmlUrlCompletedObservable
+                .Where(args => args.UserState == serverKey)
+                .Select(args => args.Result))
+                .SelectMany(uri => WebRequest.CreateHttp(uri).GetResponseAsync().ToObservable())
+                .Window(2)
+                .Select(results => 
+                    {
+                        
+                    })
+
+            _mapinfo.GetMapUrlAsync(serverKey, serverKey);
+            _mapinfo.GetXmlUrlAsync(serverKey, serverKey);
 
            // _imageHttp = (HttpWebRequest)WebRequest.CreateHttp(mapUrl.Value);
 
 
             throw new NotImplementedException();
         }
-
-
-        #endregion
-
-        #region Helper
-
 
         private static IObservable<T> singleResultObservable<T>(IObservable<T> source)
         {
@@ -86,7 +98,5 @@ namespace DiversityPhone.Services
             res.Connect();
             return res;
         }
-
-        #endregion
     }
 }
