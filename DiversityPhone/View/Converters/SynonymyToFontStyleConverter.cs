5708cb253fbbd37d3931f6e391425dd781d92f2d
Georg Rollinger <theotherspriest@web.de>
2012-12-08 23:38:58 +0100

    Display Synonymy as FontStyle

---
+++ DiversityPhone/View/Converters/SynonymyToFontStyleConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Data;
+using DiversityPhone.ViewModels;
+using System.Windows.Media;
+using DiversityPhone.Model;
+using System.Windows;
+using System.Linq;
+
+namespace DiversityPhone.View
+{
+
+    public class SynonymyToFontStyleConverter : IValueConverter
+    {    
+
+        public SynonymyToFontStyleConverter()
+        {
+            
+                
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {                        
+            if (!(value.GetType().ToString().Contains(typeof(Synonymy).ToString()))) //Design Time Data
+                throw new NotSupportedException(value.GetType().ToString());
+            
+            switch ((Synonymy)Enum.Parse(typeof(Synonymy),value.ToString(), false))
+            {
+                case Synonymy.Accepted:
+                case Synonymy.WorkingName:
+                    return FontStyles.Normal;
+                case Synonymy.Synonym:
+                    return FontStyles.Italic;
+                default:
+                    return FontStyles.Normal;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }   
+    }
+}
