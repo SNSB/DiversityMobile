8c3e9997194a076806f60dcf99f0bb77b4f990cc
Georg Rollinger <georg.rollinger@web.de>
2013-05-09 17:41:07 +0200

    Thumbnail Generation done via a Converter now. Don't keep the full images in memory.

---
+++ DiversityPhone/View/Converters/ThumbNailConverter.cs
@@ -0,0 +1,66 @@
+﻿using ReactiveUI;
+using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DiversityPhone.View
+{
+    public class ThumbNailConverter : IValueConverter
+    {
+        const int THUMBNAIL_HEIGHT = 40,
+                  THUMBNAIL_WIDTH = 40,
+                  MAX_THUMBNAILS = 100;
+
+        private MemoizingMRUCache<string, ImageSource> thumbCache;
+
+        private ImageSource loadThumb(string uri, object _)
+        {
+
+            if (!string.IsNullOrEmpty(uri))
+            {
+                using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    if (isf.FileExists(uri))
+                    {
+                        using (IsolatedStorageFileStream isfs = isf.OpenFile(uri, FileMode.Open, FileAccess.Read))
+                        {
+                            var fullScaleImage = new BitmapImage();
+                            fullScaleImage.SetSource(isfs);
+
+
+                            var thumbnailBitmap = new WriteableBitmap(fullScaleImage).Resize(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, WriteableBitmapExtensions.Interpolation.NearestNeighbor);
+
+                            return thumbnailBitmap;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public ThumbNailConverter()
+        {
+            thumbCache = new MemoizingMRUCache<string, ImageSource>(loadThumb, MAX_THUMBNAILS);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string imagePath = value as string;
+
+            if (imagePath != null)
+            {
+                return thumbCache.Get(imagePath);
+            }
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- DiversityPhone/ViewModels/Elements/MultimediaObjectVM.cs
+++ DiversityPhone/ViewModels/Elements/MultimediaObjectVM.cs
@@ -1,56 +1,11 @@
-﻿
+﻿using DiversityPhone.Model;
 using ReactiveUI;
-using DiversityPhone.Model;
-using System.Windows.Media.Imaging;
-using System.IO.IsolatedStorage;
-using System.IO;
-using System.Reactive.Linq;
-using System;
 
 
 namespace DiversityPhone.ViewModels
 {
     public class MultimediaObjectVM : ReactiveObject, IElementVM<MultimediaObject>
     {
-        private static MemoryStream load_thumb(MultimediaObject mmo)
-        {
-            switch (mmo.MediaType)
-            {
-                case MediaType.Image:
-                    if (!string.IsNullOrEmpty(mmo.Uri))
-                    {
-                        using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
-                        {
-                            if (isf.FileExists(mmo.Uri))
-                            {
-                                using (IsolatedStorageFileStream isfs = isf.OpenFile(mmo.Uri, FileMode.Open, FileAccess.Read))
-                                {
-                                    if (isfs.Length > int.MaxValue)
-                                        throw new ArgumentException("file too big");
-
-                                    var memory = new MemoryStream((int)isfs.Length);
-                                    isfs.CopyTo(memory);
-                                    return memory;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case MediaType.Audio:
-                    break;
-                case MediaType.Video:
-                    break;
-                default:
-                    break;
-            }
-                
-            
-            
-            return null;
-        }
-        private static ObservableAsyncMRUCache<MultimediaObject, MemoryStream> thumbnails = new ObservableAsyncMRUCache<MultimediaObject, MemoryStream>(mmo => Observable.Start(() => load_thumb(mmo)), 10);
-        
-
         public MultimediaObject Model
         {
             get;
@@ -59,7 +14,7 @@ namespace DiversityPhone.ViewModels
 
         public string Description { get { return Model.MediaType.ToString(); } }
 
-        //General Implementation
+
         public Icon Icon
         {
             get
@@ -78,32 +33,14 @@ namespace DiversityPhone.ViewModels
             }
         }
 
-        private BitmapImage _Thumbnail;
-        //Implementation as String for beeing able to display thumbs
-        public BitmapImage Thumbnail
-        {
-            get { return _Thumbnail;  }
-
-            set { this.RaiseAndSetIfChanged(x => x.Thumbnail, ref _Thumbnail, value); }
-        }
-
         object IElementVM.Model
         {
             get { return Model; }
         }
 
-        public MultimediaObjectVM( MultimediaObject model)
+        public MultimediaObjectVM(MultimediaObject model)
         {
             Model = model;
-
-            Model.ObservableForProperty(x => x.Uri)                
-                .Select(_ => Model)
-                .StartWith(Model)                
-                .SelectMany(mmo => thumbnails.AsyncGet(mmo))
-                .Where(thumb => thumb != null)
-                .ObserveOnDispatcher()
-                .Select(thumb => { var img = new BitmapImage(); img.SetSource(thumb); return img; })                
-                .Subscribe(img => Thumbnail = img);
-        }        
+        }
     }
 }
