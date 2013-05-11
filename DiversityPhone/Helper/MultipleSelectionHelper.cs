54e5c335267cfcdd3ba7dc99bfe7910e5530e8a6
Georg Rollinger <georg.rollinger@web.de>
2013-05-11 22:15:47 +0200

    Added a Control to select multiple entries from a list.

---
+++ DiversityPhone/Helper/MultipleSelectionHelper.cs
@@ -0,0 +1,130 @@
+﻿using ReactiveUI;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+using System.Linq;
+using System;
+using System.Reactive.Concurrency;
+using System.Diagnostics.Contracts;
+using System.Reactive;
+using System.Threading;
+
+namespace DiversityPhone.ViewModels
+{
+    public interface ISelectable<T>
+    {
+        T Value { get; }
+        bool IsSelected { get; set; }
+    }
+
+    public class MultipleSelectionHelper<T> : ReactiveObject, IObserver<IObservable<T>>
+    {
+        public readonly IScheduler Dispatcher;
+
+        private class Selectable<U> : ReactiveObject, ISelectable<U>
+        {
+            public U Value { get; private set; }
+
+            private bool _IsSelected;
+            public bool IsSelected
+            {
+                get
+                {
+                    return _IsSelected;
+                }
+                set
+                {
+                    this.RaiseAndSetIfChanged(x => x.IsSelected, ref _IsSelected, value);
+                }
+            }
+
+            public Selectable(MultipleSelectionHelper<U> owner, U value)
+            {
+                Value = value;
+                IsSelected = false;
+
+            }
+        }
+
+        private ReactiveCollection<T> Items { get; set; }
+
+        ReactiveDerivedCollection<ISelectable<T>> _SelectableItems;
+        public IEnumerable<ISelectable<T>> SelectableItems { get { return _SelectableItems; } }
+
+        public bool IsSelecting { get; set; }
+
+
+        public IObservable<IEnumerable<T>> SelectedItems
+        {
+            get
+            {
+                return Observable.Defer(() =>
+                   Observable.Start<IEnumerable<T>>(() => (from i in _SelectableItems
+                                                           where !IsSelecting || i.IsSelected
+                                                           select i.Value).ToList(),
+                                                           Dispatcher));
+            }
+        }
+
+
+        public IObservable<IEnumerable<T>> UnSelectedItems
+        {
+            get
+            {
+                return Observable.Defer(() =>
+                    Observable.Start<IEnumerable<T>>(() => (from i in _SelectableItems
+                                                            where IsSelecting && !i.IsSelected
+                                                            select i.Value).ToList(),
+                                                            Dispatcher));
+            }
+        }
+
+        readonly ISubject<IObservable<T>> _ItemStreams = new Subject<IObservable<T>>();
+
+        public MultipleSelectionHelper(
+            [Dispatcher] IScheduler Dispatcher = null
+            )
+        {
+            Dispatcher = Dispatcher ?? DispatcherScheduler.Current;
+            this.Dispatcher = Dispatcher;
+
+            Items = new ReactiveCollection<T>();
+
+
+            _SelectableItems = Items.CreateDerivedCollection(x => new Selectable<T>(this, x) as ISelectable<T>);
+
+            _ItemStreams
+                .ObserveOn(Dispatcher)
+                .Do(_ => Items.Clear())
+                .Select(stream => stream.TakeUntil(_ItemStreams))
+                .Switch()
+                .ObserveOn(Dispatcher)
+                .Subscribe(Items.Add);
+        }
+
+        public void Add(T item)
+        {
+            Dispatcher.Schedule(() => Items.Add(item));
+        }
+
+        public void Remove(T item)
+        {
+            Dispatcher.Schedule(() => Items.Remove(item));
+        }
+
+        public void OnCompleted()
+        {
+            _ItemStreams.OnCompleted();
+        }
+
+        public void OnError(Exception exception)
+        {
+            _ItemStreams.OnError(exception);
+        }
+
+        public void OnNext(IObservable<T> value)
+        {
+            _ItemStreams.OnNext(value);
+        }
+    }
+}
---
+++ DiversityPhone/View/Controls/ListBoxSelector.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Collections;
+using System.Windows.Data;
+
+namespace DiversityPhone.View
+{
+    public partial class ListBoxSelector : UserControl
+    {
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(ListBoxSelector), new PropertyMetadata(null, (sender, args) => { }));
+
+
+
+
+        public int SelectionWidth
+        {
+            get { return (int)GetValue(SelectionWidthProperty); }
+            set { SetValue(SelectionWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectionWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectionWidthProperty =
+            DependencyProperty.Register("SelectionWidth", typeof(int), typeof(ListBoxSelector), new PropertyMetadata(0));
+
+
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(ListBoxSelector), new PropertyMetadata(null, (sender, args) => { }));
+
+
+
+        public bool IsSelecting
+        {
+            get { return (bool)GetValue(IsSelectingProperty); }
+            set { SetValue(IsSelectingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelecting.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectingProperty =
+            DependencyProperty.Register("IsSelecting", typeof(bool), typeof(ListBoxSelector), new PropertyMetadata(false, (sender, args) =>
+            {
+                if (sender is ListBoxSelector)
+                {
+                    (sender as ListBoxSelector).SetSelectingState((bool)args.NewValue);
+                }
+            }));
+
+
+
+        public ListBoxSelector()
+        {
+            InitializeComponent();
+
+            SelectionMode.CurrentStateChanged += (sender, args) =>
+            {
+                touchableEdge.IsHitTestVisible = (args.NewState == Normal);
+            };
+
+            IsSelecting = false;
+
+            var itemsSourceDefaultBinding = new Binding()
+            {
+                Mode = BindingMode.OneWay,
+                Path = new PropertyPath("DataContext.SelectableItems"),
+                Source = this                
+            };
+            this.SetBinding(ItemsSourceProperty, itemsSourceDefaultBinding);
+
+            var isSelectingDefaultBinding = new Binding()
+            {
+                Mode = BindingMode.TwoWay,
+                Path = new PropertyPath("DataContext.IsSelecting"),
+                Source = this
+            };
+            this.SetBinding(IsSelectingProperty, isSelectingDefaultBinding);
+
+        }      
+        
+
+        private void EnterSelecting(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            this.IsSelecting = true;
+        }
+
+        private void SetSelectingState(bool value)
+        {
+            VisualStateManager.GoToState(this, (value) ? Selecting.Name : Normal.Name, true);
+        }
+
+    }
+}
