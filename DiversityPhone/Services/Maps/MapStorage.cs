f4967ceccce39dd271a0275e3c7cd4748b8c3b07
Georg Rollinger <rthundr@live.de>
2013-10-22 07:41:53 -0300

    Fix Map Deletion

--- DiversityPhone/Services/Maps/MapStorage.cs
+++ DiversityPhone/Services/Maps/MapStorage.cs
@@ -8,15 +8,11 @@ using System.Reactive;
 using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 
-namespace DiversityPhone.Services
-{
-    class MapDataContext : DataContext
-    {
+namespace DiversityPhone.Services {
+    class MapDataContext : DataContext {
         public MapDataContext(string fileOrConnection)
-            : base(fileOrConnection)
-        {
-            if (!this.DatabaseExists())
-            {
+            : base(fileOrConnection) {
+            if (!this.DatabaseExists()) {
                 this.CreateDatabase();
             }
         }
@@ -26,63 +22,49 @@ namespace DiversityPhone.Services
 #pragma warning restore 0649
     }
 
-    public class MapStorageService : IMapStorageService
-    {
+    public class MapStorageService : IMapStorageService {
         private const string MapFolder = "Maps";
         private const string MapDB = "Maps.sdf";
 
-        public MapStorageService()
-        {
-            using (var iso = IsolatedStorageFile.GetUserStoreForApplication())
-            {
+        public MapStorageService() {
+            using (var iso = IsolatedStorageFile.GetUserStoreForApplication()) {
                 if (!iso.DirectoryExists(MapFolder))
                     iso.CreateDirectory(MapFolder);
             }
         }
 
-        private string fileNameForMap(Map map)
-        {
+        private string fileNameForMap(Map map) {
             return string.Format("{0}/{1}.png", MapFolder, map.ServerKey);
         }
 
-        private MapDataContext getContext()
-        {
+        private MapDataContext getContext() {
             return new MapDataContext(string.Format("isostore:/{0}/{1}", MapFolder, MapDB));
         }
 
-        public IList<Map> getAllMaps()
-        {
-            using (var ctx = getContext())
-            {
+        public IList<Map> getAllMaps() {
+            using (var ctx = getContext()) {
                 return ctx.Maps.ToList();
             }
         }
 
-        public IObservable<Unit> addMap(Map map, System.IO.Stream mapContent)
-        {
-            Func<Task> impl = async () =>
-            {
-                try
-                {
-                    using (var iso = IsolatedStorageFile.GetUserStoreForApplication())
-                    {
+        public IObservable<Unit> addMap(Map map, System.IO.Stream mapContent) {
+            Func<Task> impl = async () => {
+                try {
+                    using (var iso = IsolatedStorageFile.GetUserStoreForApplication()) {
                         var filename = fileNameForMap(map);
                         if (iso.FileExists(filename))
                             iso.DeleteFile(filename);
 
-                        using (var file = iso.CreateFile(filename))
-                        {
+                        using (var file = iso.CreateFile(filename)) {
                             await mapContent.CopyToAsync(file, 4 * 1024 * 1024);
                         }
                     }
                 }
-                catch (IsolatedStorageException)
-                {
+                catch (IsolatedStorageException) {
                     mapContent.Dispose();
                 }
 
-                using (var ctx = getContext())
-                {
+                using (var ctx = getContext()) {
                     ctx.Maps.InsertOnSubmit(map);
                     ctx.SubmitChanges();
                 }
@@ -91,41 +73,34 @@ namespace DiversityPhone.Services
             return impl().ToObservable();
         }
 
-        public void ClearMaps()
-        {
-            using (var iso = IsolatedStorageFile.GetUserStoreForApplication())
-            {
-                if (iso.DirectoryExists(MapFolder))
-                {
+        public void ClearMaps() {
+            using (var iso = IsolatedStorageFile.GetUserStoreForApplication()) {
+                if (iso.DirectoryExists(MapFolder)) {
                     iso.DeleteDirectory(MapFolder);
                 }
                 iso.CreateDirectory(MapFolder);
             }
 
-            using (var ctx = getContext())
-            {
+            using (var ctx = getContext()) {
                 ctx.DeleteDatabase();
             }
         }
 
-        public void deleteMap(Map map)
-        {
-            using (var iso = IsolatedStorageFile.GetUserStoreForApplication())
-            {
+        public void deleteMap(Map map) {
+            using (var iso = IsolatedStorageFile.GetUserStoreForApplication()) {
                 var filename = fileNameForMap(map);
                 if (iso.FileExists(filename))
                     iso.DeleteFile(filename);
             }
 
-            using (var ctx = getContext())
-            {
+            using (var ctx = getContext()) {
+                ctx.Maps.Attach(map);
                 ctx.Maps.DeleteOnSubmit(map);
                 ctx.SubmitChanges();
             }
         }
 
-        public System.IO.Stream loadMap(Map map)
-        {
+        public System.IO.Stream loadMap(Map map) {
 #if false   
             var splash = "Images/AppIcons/DivMob_QuadratEnd_s_Splash.png";
             var resource = App.GetResourceStream(new Uri(splash, UriKind.Relative));
@@ -133,8 +108,7 @@ namespace DiversityPhone.Services
 #endif
             var iso = IsolatedStorageFile.GetUserStoreForApplication();
             var filename = fileNameForMap(map);
-            if (iso.FileExists(filename))
-            {
+            if (iso.FileExists(filename)) {
                 return iso.OpenFile(filename, System.IO.FileMode.Open);
             }
             else
