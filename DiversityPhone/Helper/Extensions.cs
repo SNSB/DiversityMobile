SNSB/DiversityMobile

commit 9f2638e5f57bb452cb00d9cb41300f074f54e670

Fix Migration from 0.9.9.0
Fixes #71

- Add Migration for AppSettings from 0.9.9.0 Format
- Wait for any "load settings" operations to complete, before showing setup
- During migration, add GeoPointForSeries Table, if necessary

DiversityPhone/Services/Storage/BackupService.cs
@@ -1,20 +1,21 @@
-﻿using DiversityPhone.Interface;
-using DiversityPhone.Model;
-using Microsoft;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Globalization;
-using System.IO;
-using System.IO.IsolatedStorage;
-using System.Linq;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-
-namespace DiversityPhone.Services
+﻿namespace DiversityPhone.Services
 {
+    using DiversityPhone.Interface;
+    using DiversityPhone.Model;
+    using Microsoft;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Globalization;
+    using System.IO;
+    using System.IO.IsolatedStorage;
+    using System.Linq;
+    using System.Reactive.Concurrency;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using System.Threading.Tasks;
+    using System.Xml.Serialization;
+
     public class Snapshot
     {
         public string UserName { get; set; }
@@ -90,7 +91,9 @@ namespace DiversityPhone.Services
 
             Progress.Report(Tuple.Create(BackupStage.AppData, 0.0));
 
-            var currentSettings = Settings.CurrentSettings;
+            // Blocks, but without await support for Observables it's the best we can do
+            // Not too evil, because we are on a background thread
+            var currentSettings = Settings.CurrentSettings().First();
 
             var currentProfile = Profile.CurrentProfilePath();
             var snapshotDir = GetSnapshotPath(currentSettings);

CommonAssemblyInfo.cs
@@ -20,6 +20,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
+
+// DONT FORGET TO UPDATE THE CURRENT_VERSION IN VersionMigration!!!
 [assembly: AssemblyVersion("0.9.10.0")]
 [assembly: AssemblyFileVersion("0.9.10.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

DiversityPhone.Interface/ISettingsService.cs
@@ -7,7 +7,7 @@ namespace DiversityPhone.Interface
     {
         IObservable<Settings> SettingsObservable();
 
-        Settings CurrentSettings { get; }
+        IObservable<Settings> CurrentSettings();
 
         void SaveSettings(Settings settings);
 

DiversityPhone.ServiceReference/DiversityPhone.ServiceReference.csproj
@@ -287,6 +287,8 @@
     </PropertyGroup>
     <Error Condition="!Exists('..\packages\Microsoft.Bcl.Build.1.0.21\build\Microsoft.Bcl.Build.targets')" Text="$([System.String]::Format('$(ErrorText)', '..\packages\Microsoft.Bcl.Build.1.0.21\build\Microsoft.Bcl.Build.targets'))" />
   </Target>
+  <Import Project="..\packages\Microsoft.Bcl.Build.1.0.14\tools\Microsoft.Bcl.Build.targets" Condition="Exists('..\packages\Microsoft.Bcl.Build.1.0.14\tools\Microsoft.Bcl.Build.targets')" />
+ 
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
        Other similar extension points exist, see Microsoft.Common.targets.
   <Target Name="BeforeBuild">

DiversityPhone/Helper/DatabaseMigration.cs
@@ -9,7 +9,7 @@
 
     public static partial class VersionMigration
     {
-        public static readonly Version CURRENT_VERSION = new Version(0, 9, 9, 1);
+        public static readonly Version CURRENT_VERSION = new Version(0, 9, 10, 0);
         public const int CURRENT_SCHEMA_VERSION = 1; // As of Version 0.9.9.1
 
         public static Task CreateOrUpgradeSchema(string dbPath = null, Version targetVersion = null)
@@ -31,7 +31,7 @@
                         var schema = ctx.CreateDatabaseSchemaUpdater();
                         if (schema.DatabaseSchemaVersion != CURRENT_SCHEMA_VERSION)
                         {
-                            ApplyMigrations(schema, targetVersion);
+                            ApplyMigrations(schema, targetVersion ?? CURRENT_VERSION);
                             schema.Execute();
                         }
                     }
@@ -51,6 +51,7 @@
                 }
                 if (targetVersion >= new Version(0, 9, 9, 1))
                 {
+                    schema.AddTable<GeoPointForSeries>();
                     schema.DatabaseSchemaVersion = 1;
                 }
             }

DiversityPhone/Helper/VersionMigration.cs
@@ -15,6 +15,8 @@ namespace DiversityPhone.Helper
     using System.Linq.Expressions;
     using System.Reflection;
     using System.Threading.Tasks;
+    using System.Xml;
+    using System.Xml.Serialization;
 
     public static partial class VersionMigration
     {
@@ -44,10 +46,12 @@ namespace DiversityPhone.Helper
                     await MoveDatabaseToProfile(currentProfile);
                     MoveVocabularyToProfile(currentProfile);
                     MoveMultimediaToProfile(currentProfile);
+
+                    lastVersion = new Version(0, 9, 9, 1);
                 }
-                else if (lastVersion < currentVersion)
+                if (lastVersion < currentVersion)
                 {
-                    // Nothing so far :)
+                    await CreateOrUpgradeSchema();
                 }
 
                 if (IsolatedStorageSettings.ApplicationSettings.Contains(LAST_VERSION))
@@ -383,30 +387,64 @@ namespace DiversityPhone.Helper
 
         private static void MigrateSettingsFromApplicationSettings()
         {
-#pragma warning disable 0436
-            // AppSettings in DiversityPhone Assembly (before 0.9.8)
-            try
+            var svc = App.Kernel.Get<ISettingsService>() as SettingsService;
+            var isoSettings = IsolatedStorageSettings.ApplicationSettings;
+
+            if (isoSettings.Contains("Settings"))
             {
-                AppSettings settings;
-                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<AppSettings>("Settings", out settings))
+#pragma warning disable 0436
+                // AppSettings in DiversityPhone Assembly (before 0.9.8)
+                try
                 {
-                    var svc = App.Kernel.Get<ISettingsService>();
-                    svc.SaveSettings(AppSettingsMixin.ToSettings(settings));
+                    AppSettings settings;
+                    if (isoSettings.TryGetValue<AppSettings>("Settings", out settings))
+                    {
+                        svc.SaveSettings(AppSettingsMixin.ToSettings(settings));
+                        IsolatedStorageSettings.ApplicationSettings.Remove("Settings");
+                        IsolatedStorageSettings.ApplicationSettings.Save();
+                    }
+                }
+                catch (InvalidCastException) { }
+#pragma warning restore 0436
+
+                // Appsettings in Model Assembly (in 0.9.8)
+                Model::DiversityPhone.Model.AppSettings settings2;
+                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<Model::DiversityPhone.Model.AppSettings>("Settings", out settings2))
+                {
+                    svc.SaveSettings(Model::DiversityPhone.Model.AppSettingsMixin.ToSettings(settings2));
                     IsolatedStorageSettings.ApplicationSettings.Remove("Settings");
                     IsolatedStorageSettings.ApplicationSettings.Save();
                 }
             }
-            catch (InvalidCastException) { }
-#pragma warning restore 0436
-
-            // Appsettings in Model Assembly (in 0.9.8)
-            Model::DiversityPhone.Model.AppSettings settings2;
-            if (IsolatedStorageSettings.ApplicationSettings.TryGetValue<Model::DiversityPhone.Model.AppSettings>("Settings", out settings2))
+            else
             {
-                var svc = App.Kernel.Get<ISettingsService>();
-                svc.SaveSettings(Model::DiversityPhone.Model.AppSettingsMixin.ToSettings(settings2));
-                IsolatedStorageSettings.ApplicationSettings.Remove("Settings");
-                IsolatedStorageSettings.ApplicationSettings.Save();
+                // Appsettings in Model Assembly (in 0.9.8)
+                // Stored in the Profile Folder
+                var settingsPath = svc.GetSettingsPath();
+
+                try
+                {
+                    using (var iso = IsolatedStorageFile.GetUserStoreForApplication())
+                    {
+                        if (iso.FileExists(settingsPath))
+                        {
+                            using (var settingsFile = iso.OpenFile(settingsPath, FileMode.Open, FileAccess.Read))
+                            {
+                                Model::DiversityPhone.Model.AppSettings settings2;
+                                var SettingsSerializer = new XmlSerializer(typeof(Model::DiversityPhone.Model.AppSettings));
+
+                                var settingsXml = XmlReader.Create(settingsFile);
+                                if (SettingsSerializer.CanDeserialize(settingsXml))
+                                {
+                                    settings2 = (Model::DiversityPhone.Model.AppSettings)SettingsSerializer.Deserialize(settingsXml);
+
+                                    svc.SaveSettings(Model::DiversityPhone.Model.AppSettingsMixin.ToSettings(settings2));
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception) { /*Ah well*/ }
             }
         }
     }

DiversityPhone/Services/Storage/SettingsService.cs
@@ -8,6 +8,7 @@ using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Threading;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -17,6 +18,7 @@ namespace DiversityPhone.Services
     {
         public const string SETTINGS_FILE = "AppSettings.xml";
 
+        private readonly IScheduler Dispatcher;
         private readonly ICurrentProfile Profile;
         private readonly XmlSerializer SettingsSerializer;
 
@@ -24,7 +26,11 @@ namespace DiversityPhone.Services
 
         private ISubject<Settings> _SettingsIn;
         private ISubject<Settings> _SettingsOut = new Subject<Settings>();
-        private IObservable<Settings> _SettingsReplay;
+        private IObservable<Settings> _SettingsDispatcher;
+
+        private BehaviorSubject<Settings> _SettingsMostRecent = new BehaviorSubject<Settings>(null);
+
+        private volatile int _LoadsInFlight = 0;
 
         public SettingsService(
             ICurrentProfile Profile,
@@ -36,33 +42,32 @@ namespace DiversityPhone.Services
             Contract.Requires(Dispatcher != null);
             Contract.Requires(ThreadPool != null);
 
+            this.Dispatcher = Dispatcher;
             this.Profile = Profile;
             this.SettingsSerializer = new XmlSerializer(typeof(Settings));
 
-            _SettingsReplay = _SettingsOut
-                .Merge(
-                    _ReloadSettings
-                    .Select(x => null as Settings)
-                )
-                .ObserveOn(Dispatcher)
-                .Replay(1)
-                .PermaRef();
+            _SettingsDispatcher =
+                _SettingsOut
+                .ObserveOn(Dispatcher);
+
+            _SettingsOut
+                .Subscribe(_SettingsMostRecent);
 
             _SettingsIn = new Subject<Settings>();
 
             _SettingsIn
                 .ObserveOn(ThreadPool)
                 .Select(PersistSettings)
-                .ObserveOn(Dispatcher)
                 .Subscribe(_SettingsOut);
 
             Profile
                 .CurrentProfilePathObservable()
                 .Merge(_ReloadSettings.Select(_ => Profile.CurrentProfilePath()))
+                .Do(EnterLoad)
                 .Select(ProfileToSettingsPath)
                 .ObserveOn(ThreadPool)
                 .Select(LoadSettingsFromFile)
-                .ObserveOn(Dispatcher)
+                .Do(ExitLoad)
                 .Subscribe(_SettingsOut);
         }
 
@@ -123,7 +128,28 @@ namespace DiversityPhone.Services
             return s;
         }
 
-        private string GetSettingsPath()
+        private void EnterLoad(object _)
+        {
+            lock (this)
+            {
+                _LoadsInFlight++;
+            }
+        }
+
+        private void ExitLoad(object _)
+        {
+            lock (this)
+            {
+                if (_LoadsInFlight <= 0)
+                {
+                    throw new InvalidOperationException("ExitLoad called more often than EnterLoad");
+                }
+
+                _LoadsInFlight--;
+            }
+        }
+
+        public string GetSettingsPath()
         {
             var currentProfile = Profile.CurrentProfilePath();
             return ProfileToSettingsPath(currentProfile);
@@ -146,15 +172,22 @@ namespace DiversityPhone.Services
 
         public IObservable<UserCredentials> CurrentCredentials()
         {
-            return _SettingsReplay
+            return _SettingsDispatcher
                 .Select(s => s.ToCreds());
         }
 
         public IObservable<Settings> SettingsObservable()
         {
-            return _SettingsReplay;
+            return _SettingsDispatcher;
         }
 
-        public Settings CurrentSettings { get { return _SettingsReplay.FirstOrDefault(); } }
+        public IObservable<Settings> CurrentSettings()
+        {
+            // Block until all Loads have finished, then return most recent Settings
+            return _SettingsMostRecent
+                .SkipWhile(_ => _LoadsInFlight != 0)
+                .FirstAsync()
+                .ObserveOn(Dispatcher);
+        }
     }
 }

DiversityPhone/ViewModels/Utility/SetupVM.cs
@@ -168,15 +168,14 @@
             // If There already is a configuration (Settings)
             // Go To Home Page
             this.FirstActivation()
-                .Zip(Settings.SettingsObservable(), (_, s) => s != null)
-                .Select(x => (x) ? Page.Home : Page.SetupWelcome)
+                .SelectMany(_ => Settings.CurrentSettings())
+                .Select(x => (x != null) ? Page.Home : Page.SetupWelcome)
                 .ToMessage(Messenger);
 
             _IsOnlineAvailable = this.ObservableToProperty(Connectivity.WifiAvailable(), x => x.IsOnlineAvailable, false, Dispatcher);
 
             // Show current login data in case of Reset
             Settings.SettingsObservable()
-                .ObserveOn(Dispatcher)
                 .Subscribe(SetLogin);
 
             // Command To begin Setup

commit f8b55908101421a07c84e81cfb8edebfa286131c

Fix Deadlock when taking a snapshot
Introduced in 9f2638e
References #71

DiversityPhone/Helper/Extensions.cs
@@ -5,6 +5,8 @@
     using System.Collections.Generic;
     using System.Diagnostics.Contracts;
     using System.Reactive.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     public class DispatcherAttribute : Attribute { }
 
@@ -111,6 +113,30 @@
             {
                 return default(T);
             }
+        }
+
+        public static Task<T> ToTask<T>(this IObservable<T> This)
+        {
+            return ToTask(This, CancellationToken.None);
+        }
+
+        public static Task<T> ToTask<T>(this IObservable<T> This, CancellationToken cancellationToken)
+        {
+            Contract.Requires(This != null);
+
+            var completion = new TaskCompletionSource<T>();
+
+            var subscription = This
+                .LastAsync()
+                .Subscribe(
+                    (x) => completion.TrySetResult(x),
+                    (err) => completion.TrySetException(err),
+                    () => { }
+                );
+
+            cancellationToken.Register(subscription.Dispose, useSynchronizationContext: false);
+
+            return completion.Task;
         }
     }
 }

DiversityPhone/Services/Storage/BackupService.cs
@@ -91,9 +91,7 @@
 
             Progress.Report(Tuple.Create(BackupStage.AppData, 0.0));
 
-            // Blocks, but without await support for Observables it's the best we can do
-            // Not too evil, because we are on a background thread
-            var currentSettings = Settings.CurrentSettings().First();
+            var currentSettings = await Settings.CurrentSettings().ToTask();
 
             var currentProfile = Profile.CurrentProfilePath();
             var snapshotDir = GetSnapshotPath(currentSettings);